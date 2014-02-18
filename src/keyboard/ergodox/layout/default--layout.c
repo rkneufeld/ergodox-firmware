f57acc892afe1b568659118e585643c3e4c4e7e6
Ryan Neufeld <ryan@rkn.io>
2014-02-18 08:31:18 -0500

    Make RHS start at 6, put dash in backspace posn

--- src/keyboard/ergodox/layout/default--layout.c
+++ src/keyboard/ergodox/layout/default--layout.c
@@ -75,7 +75,7 @@ KB_MATRIX_LAYER(
 	0,	0,	0,
 	KEY_DeleteBackspace,	KEY_DeleteForward,	0,
 	// right hand
-	KEY_7_Ampersand,	KEY_8_Asterisk,	KEY_9_LeftParenthesis,	KEY_0_RightParenthesis,	KEY_Dash_Underscore,	KEY_Equal_Plus,	KEY_DeleteBackspace,
+	KEY_6_Caret,	KEY_7_Ampersand,	KEY_8_Asterisk,	KEY_9_LeftParenthesis,	KEY_0_RightParenthesis,	KEY_Equal_Plus,	KEY_Dash_Underscore,
 	2,	KEY_y_Y,	KEY_u_U,	KEY_i_I,	KEY_o_O,	KEY_p_P,	KEY_Backslash_Pipe,
 	KEY_h_H,	KEY_j_J,	KEY_k_K,	KEY_l_L,	KEY_Semicolon_Colon,	KEY_SingleQuote_DoubleQuote,
 	1,	KEY_n_N,	KEY_m_M,	KEY_Comma_LessThan,	KEY_Period_GreaterThan,	KEY_Slash_Question,	KEY_RightShift,
